229597689d9e9eff6899ff894251257e5b1a50ae
MaratArkhipov <maratkzn2010@yandex.ru>
2020-09-04 15:58:50 +0400

    all done

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalculator = new IntsCalculator();
+        System.out.println(intsCalculator.pow(2, 2));
+        System.out.println(intsCalculator.mult(2, 2));
+        System.out.println(intsCalculator.sum(2, 2));
+    }
+}
